a6867b14723281a4a2326717c382655587490e49
baztuoi <arccfr@gmail.com>
2021-11-05 22:47:34 +1100

    update

--- commands/help.js
+++ commands/help.js
@@ -18,6 +18,7 @@ module.exports = {
 			.addField(`\`${thisGuild}info\``, "View the general information of NeoBot.")
 			.addField(`\`${thisGuild}feedback [text]\``, "Send a letter of encouragement or feedback to Lukey :)")
 			.addField(`\`${thisGuild}8ball [question]\``, "Test your luck with the 8ball command.")
+			.addField(`\`${thisGuild}suggest [text]\``, "Send a suggestion to the bot developer!")
 
 			message.channel.send({ embeds: [help] })
 
